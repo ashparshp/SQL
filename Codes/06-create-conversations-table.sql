6c5eb130cdf102aabd56f42003d4ab31db25f7fe
Ashparshp <ashparsh.connects@gmail.com>
2025-08-01 04:07:29 +0530

    Refactor employers and conversations tables: add yearly_revenue column and remove default values for is_hiring and date_sent

--- Codes/06-create-conversations-table.sql
+++ Codes/06-create-conversations-table.sql
@@ -2,5 +2,5 @@ CREATE TABLE conversations (
     user_name VARCHAR(255),
     employer_name VARCHAR(255),
     message TEXT,
-    date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+    date_sent TIMESTAMP
+)
--- Codes/08-default-values.sql
+++ Codes/08-default-values.sql
@@ -3,5 +3,12 @@ CREATE TABLE employers (
     company_address VARCHAR(300),
     -- yearly_revenue FLOAT(5, 2) -- Approximation, Allowed: 123.12, 12.1; Not Allowed: 123.123, 12.123, 12.1234; Approximate, stores up to 5 digits with 2 after decimal. Extra decimal digits are rounded (e.g., 123.4567 becomes 123.46). May have precision issues.
     yearly_revenue NUMERIC(5, 2), -- Exact value, Allowed: 123.12, 12.1; Not Allowed: 123.123, 12.123, 12.1234; Exact, stores up to 5 digits with 2 after decimal. Rejects values with more than 2 decimal digits or more than 3 digits before decimal.
-    is_hiring BOOLEAN
+    is_hiring BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE conversations (
+    user_name VARCHAR(255),
+    employer_name VARCHAR(255),
+    message TEXT,
+    date_sent TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
