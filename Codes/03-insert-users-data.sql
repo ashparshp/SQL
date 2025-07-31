a81382c2763062c4c450ca8e31f14789c36b12b5
Ashparshp <ashparsh.connects@gmail.com>
2025-08-01 03:54:59 +0530

    Add SQL script to insert user data into the users table

---
+++ Codes/03-insert-users-data.sql
@@ -0,0 +1,16 @@
+-- Single row
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (19000, 'Sparsh Pandey', 'self-employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (30000, 'Rohit Sharma', 'self-employed');
+
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES (0, 'Virat Kohli', 'unemployed');
+
+-- Multiple rows
+INSERT INTO users (yearly_salary, full_name, current_status)
+VALUES 
+(25000, 'Sachin Tendulkar', 'self-employed'),
+(15000, 'MS Dhoni', 'self-employed'),
+(16000, 'Rishabh Pant', 'self-employed');
