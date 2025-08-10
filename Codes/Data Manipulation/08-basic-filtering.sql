8e6b0ccad660f15cd4eec267fe7ea64511b0fe2f
Ashparshp <ashparsh.connects@gmail.com>
2025-08-11 03:51:39 +0530

    Add additional filtering SQL queries for recurring sales analysis

--- Codes/Data Manipulation/08-basic-filtering.sql
+++ Codes/Data Manipulation/08-basic-filtering.sql
@@ -5,3 +5,10 @@
 -- SELECT * from sales WHERE volume < 1000;
 
 -- SELECT * from sales WHERE volume <= 1000;
+
+-- SELECT * from sales WHERE is_recurring = TRUE;
+
+-- SELECT * from sales WHERE is_recurring = 0;
+
+-- SELECT * from sales WHERE is_recurring IS TRUE;
+
