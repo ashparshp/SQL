8a78e8e3bdcba887d460e43d671a01cd5fb990f7
Ashparshp <ashparsh.connects@gmail.com>
2025-08-11 04:34:13 +0530

    Add additional SQL examples for ordering and limiting sales data

--- Codes/Data Manipulation/12-order-and-limit.sql
+++ Codes/Data Manipulation/12-order-and-limit.sql
@@ -8,7 +8,13 @@
 -- ORDER BY volume DESC
 -- LIMIT 10;
 
+-- SELECT * FROM sales
+-- ORDER BY volume DESC
+-- LIMIT 10
+-- OFFSET 1;
+
 SELECT * FROM sales
+WHERE is_disputed IS FALSE
 ORDER BY volume DESC
-LIMIT 10
+LIMIT 2
 OFFSET 1;
