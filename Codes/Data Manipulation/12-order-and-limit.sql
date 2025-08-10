7565075da1f7cffab2fda35b6214375e39c7ac42
Ashparshp <ashparsh.connects@gmail.com>
2025-08-11 04:31:58 +0530

    SQL examples for ordering and limiting sales data

---
+++ Codes/Data Manipulation/12-order-and-limit.sql
@@ -0,0 +1,14 @@
+-- SELECT * FROM sales
+-- ORDER BY volume;
+
+-- SELECT * FROM sales
+-- ORDER BY volume DESC;
+
+-- SELECT * FROM sales
+-- ORDER BY volume DESC
+-- LIMIT 10;
+
+SELECT * FROM sales
+ORDER BY volume DESC
+LIMIT 10
+OFFSET 1;
