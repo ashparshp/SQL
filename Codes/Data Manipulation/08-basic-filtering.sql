2b138b769caaaa0ac3f90ab39b44049db0731f5b
Ashparshp <ashparsh.connects@gmail.com>
2025-08-11 04:00:39 +0530

    Fix SQL comments to use consistent uppercase for keywords

--- Codes/Data Manipulation/08-basic-filtering.sql
+++ Codes/Data Manipulation/08-basic-filtering.sql
@@ -1,14 +1,14 @@
--- SELECT * from sales WHERE volume > 1000;
+-- SELECT * FROM sales WHERE volume > 1000;
 
--- SELECT * from sales WHERE volume >= 1000;
+-- SELECT * FROM sales WHERE volume >= 1000;
 
--- SELECT * from sales WHERE volume < 1000;
+-- SELECT * FROM sales WHERE volume < 1000;
 
--- SELECT * from sales WHERE volume <= 1000;
+-- SELECT * FROM sales WHERE volume <= 1000;
 
--- SELECT * from sales WHERE is_recurring = TRUE;
+-- SELECT * FROM sales WHERE is_recurring = TRUE;
 
--- SELECT * from sales WHERE is_recurring = 0;
+-- SELECT * FROM sales WHERE is_recurring = 0;
 
--- SELECT * from sales WHERE is_recurring IS TRUE;
+-- SELECT * FROM sales WHERE is_recurring IS TRUE;
 
