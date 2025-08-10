ae5064a68a2da060c4a3118cadd7a918ec1d5007
Ashparshp <ashparsh.connects@gmail.com>
2025-08-11 04:04:13 +0530

    Add additional SQL comments for clarity on disputed sales filtering

--- Codes/Data Manipulation/08-basic-filtering.sql
+++ Codes/Data Manipulation/08-basic-filtering.sql
@@ -14,4 +14,8 @@
 
 -- SELECT * FROM sales WHERE (is_disputed IS TRUE) && (volume > 5000);
 
-SELECT * FROM sales WHERE (is_disputed IS TRUE) AND (volume > 5000);
+-- SELECT * FROM sales WHERE (is_disputed IS TRUE) AND (volume > 5000);
+
+-- SELECT * FROM sales WHERE (is_disputed IS TRUE) || (volume > 5000);
+
+-- SELECT * FROM sales WHERE (is_disputed IS TRUE) OR (volume > 5000);
