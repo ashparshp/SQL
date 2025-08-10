0ab0fad9221d2d30f80f876d0a2e31f47e3ff741
Ashparshp <ashparsh.connects@gmail.com>
2025-08-11 03:49:30 +0530

    Add basic filtering SQL queries for sales volume analysis

---
+++ Codes/Data Manipulation/08-basic-filtering.sql
@@ -0,0 +1,7 @@
+-- SELECT * from sales WHERE volume > 1000;
+
+-- SELECT * from sales WHERE volume >= 1000;
+
+-- SELECT * from sales WHERE volume < 1000;
+
+-- SELECT * from sales WHERE volume <= 1000;
