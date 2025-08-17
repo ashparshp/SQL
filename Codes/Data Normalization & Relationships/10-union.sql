29e9188dbd0eac275f9feb11fc2abf0e231f3d0d
Ashparshp <ashparsh.connects@gmail.com>
2025-08-17 07:45:30 +0530

    Refactor SQL union query examples for clarity and correctness

--- Codes/Data Normalization & Relationships/10-union.sql
+++ Codes/Data Normalization & Relationships/10-union.sql
@@ -1,5 +1,19 @@
-SELECT * FROM users
-WHERE id < 3
+-- SELECT * FROM users
+-- WHERE id < 3
+-- UNION
+-- SELECT * FROM users
+-- WHERE id = 5;
+
+-- -- Uisng OR
+-- SELECT * FROM users
+-- WHERE id < 3 OR id = 5;
+
+-- Error: UNION ALL cannot be used with different column counts
+-- SELECT * FROM users
+-- UNION
+-- SELECT * FROM addresses;
+
+-- Correct usage of UNION with same column count
+SELECT id, first_name FROM users
 UNION
-SELECT * FROM users
-WHERE id = 5;
+SELECT id, street FROM addresses;
