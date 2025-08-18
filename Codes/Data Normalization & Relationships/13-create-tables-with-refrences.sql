fb4bc01d9aa6531ea23659d1c53f3fe6bafa6969
Ashparshp <ashparsh.connects@gmail.com>
2025-08-18 08:55:27 +0530

    Add SQL scripts for creating tables, inserting data, and deleting related records

---
+++ Codes/Data Normalization & Relationships/13-create-tables-with-refrences.sql
@@ -0,0 +1,22 @@
+CREATE TABLE addresses (
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- PostgresSQL
+    street VARCHAR(255) NOT NULL,
+    house_number VARCHAR(50) NOT NULL,
+    city_id INT NOT NULL
+);
+
+CREATE TABLE users (
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- PostgresSQL
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    address_id INT REFERENCES addresses(id) ON DELETE RESTRICT
+);
+
+CREATE TABLE cities (
+    -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    id SERIAL PRIMARY KEY, -- PostgresSQL
+    name VARCHAR(255) NOT NULL
+);
