56a59c7521dcdbeb774206d071afa3b60e5bcf8a
Ashparshp <ashparsh.connects@gmail.com>
2025-08-18 11:23:04 +0530

    Add ORDER BY clause to employee selection query for consistent sorting by birthdate

--- Codes/Data Normalization & Relationships/25-query-data.sql
+++ Codes/Data Normalization & Relationships/25-query-data.sql
@@ -11,4 +11,5 @@ SELECT e.id AS employee_id, e.first_name, cb.name
 FROM employees AS e
 LEFT JOIN teams AS t ON t.id = e.team_id
 LEFT JOIN company_buildings as cb ON cb.id = t.building_id
-WHERE cb.id = 3;
+WHERE cb.id = 3
+ORDER BY e.birthdate;
