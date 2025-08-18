eca3059c04f6e14a16a372376001f5971a3ca58b
Ashparshp <ashparsh.connects@gmail.com>
2025-08-18 11:15:01 +0530

    Refactor SQL query to remove commented-out joins and clarify employee selection by team

--- Codes/Data Normalization & Relationships/25-query-data.sql
+++ Codes/Data Normalization & Relationships/25-query-data.sql
@@ -1,3 +1,8 @@
-SELECT * FROM employees AS e
-LEFT JOIN projects_employees AS pe ON pe.employee_id = e.id
-LEFT JOIN projects AS p ON p.id= pe.project_id;
+-- SELECT * FROM employees AS e
+-- LEFT JOIN projects_employees AS pe ON pe.employee_id = e.id
+-- LEFT JOIN projects AS p ON p.id= pe.project_id;
+
+SELECT e.id AS employee_id, e.first_name, t.name
+FROM employees AS e
+LEFT JOIN  teams AS t ON t.id = e.team_id
+WHERE t.id = 2;
