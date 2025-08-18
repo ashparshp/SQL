e342a68e291a3a42f8fd7b5695c3a51ddebc970c
Ashparshp <ashparsh.connects@gmail.com>
2025-08-18 10:59:20 +0530

    Update SQL cleanup and data insertion scripts for clarity and completeness

--- Codes/Data Normalization & Relationships/22-clean-db.sql
+++ Codes/Data Normalization & Relationships/22-clean-db.sql
@@ -2,3 +2,4 @@ DROP TABLE intranet_accounts;
 DROP TABLE employees;
 DROP TABLE teams;
 DROP TABLE company_buildings;
+DROP TABLE projects;
--- Codes/Data Normalization & Relationships/23-final-setup.sql
+++ Codes/Data Normalization & Relationships/23-final-setup.sql
@@ -1,47 +1,47 @@
--- MySQL
-CREATE TABLE projects (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(200) NOT NULL,
-    deadline DATE
-);
+-- -- MySQL
+-- CREATE TABLE projects (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     title VARCHAR(200) NOT NULL,
+--     deadline DATE
+-- );
 
 
-CREATE TABLE company_buildings (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(200) NOT NULL
-);
+-- CREATE TABLE company_buildings (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     name VARCHAR(200) NOT NULL
+-- );
 
-CREATE TABLE teams (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(200) NOT NULL,
-    building_id INT,
-    FOREIGN KEY (building_id) REFERENCES company_buildings(id) ON DELETE SET NULL
-);
+-- CREATE TABLE teams (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     name VARCHAR(200) NOT NULL,
+--     building_id INT,
+--     FOREIGN KEY (building_id) REFERENCES company_buildings(id) ON DELETE SET NULL
+-- );
 
-CREATE TABLE employees (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(200) NOT NULL,
-    last_name VARCHAR(200) NOT NULL,
-    birthdate DATE NOT NULL,
-    email VARCHAR(200) UNIQUE NOT NULL,
-    team_id INT DEFAULT 1,
-    FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE SET NULL
-);
+-- CREATE TABLE employees (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     first_name VARCHAR(200) NOT NULL,
+--     last_name VARCHAR(200) NOT NULL,
+--     birthdate DATE NOT NULL,
+--     email VARCHAR(200) UNIQUE NOT NULL,
+--     team_id INT DEFAULT 1,
+--     FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE SET NULL
+-- );
 
-CREATE TABLE intranet_accounts (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    email VARCHAR(200),
-    FOREIGN KEY (email) REFERENCES employees(email) ON DELETE CASCADE,
-    password VARCHAR(200) NOT NULL
-);
+-- CREATE TABLE intranet_accounts (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     email VARCHAR(200),
+--     FOREIGN KEY (email) REFERENCES employees(email) ON DELETE CASCADE,
+--     password VARCHAR(200) NOT NULL
+-- );
 
-CREATE TABLE projects_employees (
-   id INT PRIMARY KEY AUTO_INCREMENT,
-    employee_id INT,
-    FOREIGN KEY (employee_id) REFERENCES employees ON DELETE CASCADE,
-    project_id INT,
-    FOREIGN KEY (project_id) REFERENCES projects ON DELETE CASCADE,
-);
+-- CREATE TABLE projects_employees (
+--    id INT PRIMARY KEY AUTO_INCREMENT,
+--     employee_id INT,
+--     FOREIGN KEY (employee_id) REFERENCES employees ON DELETE CASCADE,
+--     project_id INT,
+--     FOREIGN KEY (project_id) REFERENCES projects ON DELETE CASCADE
+-- );
 
 -- PostgresSQL
 CREATE TABLE projects (
