1bbfed0ac74132fe4ca5d9713fabd3ac4cc01113
Ashparshp <ashparsh.connects@gmail.com>
2025-08-18 08:57:31 +0530

    Refactor SQL table creation scripts to uncomment and update foreign key constraints for addresses and users

--- Codes/Data Normalization & Relationships/13-create-tables-with-refrences.sql
+++ Codes/Data Normalization & Relationships/13-create-tables-with-refrences.sql
@@ -1,3 +1,26 @@
+-- CREATE TABLE addresses (
+--     -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+--     id SERIAL PRIMARY KEY, -- PostgresSQL
+--     street VARCHAR(255) NOT NULL,
+--     house_number VARCHAR(50) NOT NULL,
+--     city_id INT NOT NULL
+-- );
+
+-- CREATE TABLE users (
+--     -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+--     id SERIAL PRIMARY KEY, -- PostgresSQL
+--     first_name VARCHAR(255) NOT NULL,
+--     last_name VARCHAR(255) NOT NULL,
+--     email VARCHAR(255) NOT NULL,
+--     address_id INT REFERENCES addresses(id) ON DELETE RESTRICT -- NO ACTION is same as ON DELETE RESTRICT
+-- );
+
+-- CREATE TABLE cities (
+--     -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+--     id SERIAL PRIMARY KEY, -- PostgresSQL
+--     name VARCHAR(255) NOT NULL
+-- );
+
 CREATE TABLE addresses (
     -- id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
     id SERIAL PRIMARY KEY, -- PostgresSQL
@@ -12,7 +35,7 @@ CREATE TABLE users (
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    address_id INT REFERENCES addresses(id) ON DELETE RESTRICT
+    address_id INT REFERENCES addresses(id) ON DELETE CASCADE -- User will be deleted if address is deleted
 );
 
 CREATE TABLE cities (
