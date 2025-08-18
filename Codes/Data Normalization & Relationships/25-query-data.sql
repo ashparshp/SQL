8135f1b11d1d1ca5d84d1219d0aaada953941cae
Ashparshp <ashparsh.connects@gmail.com>
2025-08-18 11:22:13 +0530

    Refactor SQL query to uncomment and clarify employee selection by company building

--- Codes/Data Normalization & Relationships/25-query-data.sql
+++ Codes/Data Normalization & Relationships/25-query-data.sql
@@ -2,7 +2,13 @@
 -- LEFT JOIN projects_employees AS pe ON pe.employee_id = e.id
 -- LEFT JOIN projects AS p ON p.id= pe.project_id;
 
-SELECT e.id AS employee_id, e.first_name, t.name
+-- SELECT e.id AS employee_id, e.first_name, t.name
+-- FROM employees AS e
+-- LEFT JOIN  teams AS t ON t.id = e.team_id
+-- WHERE t.id = 2;
+
+SELECT e.id AS employee_id, e.first_name, cb.name
 FROM employees AS e
-LEFT JOIN  teams AS t ON t.id = e.team_id
-WHERE t.id = 2;
+LEFT JOIN teams AS t ON t.id = e.team_id
+LEFT JOIN company_buildings as cb ON cb.id = t.building_id
+WHERE cb.id = 3;
