34ac3c3d13faa32197722f432d18500aa28e9c2b
Ashparshp <ashparsh.connects@gmail.com>
2025-08-19 08:37:30 +0530

    Add SQL scripts for creating and dropping tables related to projects, employees, teams, and company buildings

---
+++ Codes/More On Primary & Foreign Keys/03-self-refrencing.sql
@@ -0,0 +1,16 @@
+--MySQL
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    supervisor_id INT,
+    FOREIGN KEY (supervisor_id) REFERENCES employees(id) ON DELETE SET NULL
+);
+
+-- PostgreSQL
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(200) NOT NULL,
+    last_name VARCHAR(200) NOT NULL,
+    supervisor_id INT REFERENCES employees(id) ON DELETE SET NULL
+);
