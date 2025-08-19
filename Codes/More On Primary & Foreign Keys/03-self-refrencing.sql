c67141939c24181b0eeb491e5b20d5743c34299d
Ashparshp <ashparsh.connects@gmail.com>
2025-08-19 08:44:04 +0530

    Fix formatting in MySQL script and add querying self-referencing data SQL script

--- Codes/More On Primary & Foreign Keys/03-self-refrencing.sql
+++ Codes/More On Primary & Foreign Keys/03-self-refrencing.sql
@@ -1,4 +1,4 @@
---MySQL
+-- MySQL
 CREATE TABLE employees (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(200) NOT NULL,
@@ -7,10 +7,10 @@ CREATE TABLE employees (
     FOREIGN KEY (supervisor_id) REFERENCES employees(id) ON DELETE SET NULL
 );
 
--- PostgreSQL
-CREATE TABLE employees (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(200) NOT NULL,
-    last_name VARCHAR(200) NOT NULL,
-    supervisor_id INT REFERENCES employees(id) ON DELETE SET NULL
-);
+-- -- PostgreSQL
+-- CREATE TABLE employees (
+--     id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(200) NOT NULL,
+--     last_name VARCHAR(200) NOT NULL,
+--     supervisor_id INT REFERENCES employees(id) ON DELETE SET NULL
+-- );
