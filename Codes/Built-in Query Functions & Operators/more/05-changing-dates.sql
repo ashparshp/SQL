a05c32676775eed720fa8db0e31523b8f425e527
Ashparshp <ashparsh.connects@gmail.com>
2025-08-20 12:43:07 +0530

    Update comments and SQL query for date manipulation in PostgreSQL

--- Codes/Built-in Query Functions & Operators/more/05-changing-dates.sql
+++ Codes/Built-in Query Functions & Operators/more/05-changing-dates.sql
@@ -1,8 +1,8 @@
 -- SELECT membership_start + 7, membership_start
--- FROM memberships;
+-- FROM memberships; -- PostgreSQL adds 7 days to the date
 
 -- SELECT DATE_ADD(membership_start, INTERVAL 7 DAY), membership_start
--- FROM memberships;
+-- FROM memberships; -- MySQL adds 7 days to the date
 
-SELECT (membership_start + INTERVAL '7 DAY')::TIMESTAMP::DATE, membership_start
-FROM memberships;
+SELECT (membership_start + INTERVAL '7 MONTHS')::TIMESTAMP::DATE, membership_start
+FROM memberships; -- PostgreSQL adds 7 months to the date
