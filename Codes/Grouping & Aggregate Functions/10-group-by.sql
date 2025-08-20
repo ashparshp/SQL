c800dce4676bc36523cb2ac998d8e2687da23520
Ashparshp <ashparsh.connects@gmail.com>
2025-08-20 08:51:59 +0530

    Refactor SQL script to comment out previous booking aggregation and maintain payment method aggregation

--- Codes/Grouping & Aggregate Functions/10-group-by.sql
+++ Codes/Grouping & Aggregate Functions/10-group-by.sql
@@ -1,6 +1,11 @@
 -- SELECT booking_date, SUM (num_guests) AS total_guests
 -- FROM bookings; -- error: missing GROUP BY clause
 
-SELECT booking_date, SUM (num_guests) AS total_guests
-FROM bookings
-GROUP BY booking_date;
+-- SELECT booking_date, SUM (num_guests) AS total_guests
+-- FROM bookings
+-- GROUP BY booking_date;
+
+SELECT p.name, SUM (b.num_guests)
+FROM payment_methods AS p
+LEFT JOIN bookings AS b ON p.id = b.payment_id
+GROUP BY p.name;
