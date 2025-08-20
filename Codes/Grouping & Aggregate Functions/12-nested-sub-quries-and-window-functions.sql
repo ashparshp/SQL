bb9829570c6905a1999bb37f817cfd1e95430618
Ashparshp <ashparsh.connects@gmail.com>
2025-08-20 11:08:56 +0530

    Refactor SQL script to uncomment and restore window function calculations for booking tips

---
+++ Codes/Grouping & Aggregate Functions/12-nested-sub-quries-and-window-functions.sql
@@ -0,0 +1,30 @@
+-- SELECT MIN (daily_sum) AS min_billed
+-- FROM (
+--     SELECT booking_date, SUM (amount_billed) AS daily_sum
+--     FROM bookings
+--     GROUP BY booking_date
+-- ) AS daily_table;
+
+-- SELECT booking_date
+-- FROM bookings
+-- GROUP BY booking_date
+-- HAVING SUM(amount_billed) = (
+--     SELECT MIN (daily_sum) AS min_billed
+--     FROM (
+--         SELECT booking_date, SUM (amount_billed) AS daily_sum
+--         FROM bookings
+--         GROUP BY booking_date
+--     ) AS daily_table
+-- );
+
+-- Window function
+-- SELECT booking_date, amount_tipped, SUM (amount_tipped) OVER()
+-- FROM bookings;
+
+-- Window function with partitioning: calculating daily total tips
+-- SELECT booking_date, amount_tipped, SUM (amount_tipped) OVER(PARTITION BY booking_date)
+-- FROM bookings;
+
+-- Window function with partitioning and ordering: calculating cumulative tips by booking date
+SELECT booking_date, amount_tipped, SUM (amount_tipped) OVER(PARTITION BY booking_date ORDER BY amount_billed ASC)
+FROM bookings;
