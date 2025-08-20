d8971c6960923798a64b96f187cccefa0eff9b8f
Ashparshp <ashparsh.connects@gmail.com>
2025-08-20 08:54:18 +0530

    Comment out previous aggregation queries and adjust GROUP BY clause for payment method and booking date

--- Codes/Grouping & Aggregate Functions/10-group-by.sql
+++ Codes/Grouping & Aggregate Functions/10-group-by.sql
@@ -5,7 +5,12 @@
 -- FROM bookings
 -- GROUP BY booking_date;
 
-SELECT p.name, SUM (b.num_guests)
+-- SELECT p.name, SUM (b.num_guests)
+-- FROM payment_methods AS p
+-- LEFT JOIN bookings AS b ON p.id = b.payment_id
+-- GROUP BY p.name;
+
+SELECT p.name, b.booking_date, SUM (b.num_guests)
 FROM payment_methods AS p
 LEFT JOIN bookings AS b ON p.id = b.payment_id
-GROUP BY p.name;
+GROUP BY p.name, b.booking_date;
