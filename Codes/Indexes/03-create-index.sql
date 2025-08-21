88d8b7466ff3a7fe83241937d6aba364fa21b4f3
Ashparshp <ashparsh.connects@gmail.com>
2025-08-21 06:36:42 +0530

    Add unique index creation for users' salary to prevent duplicate entries

--- Codes/Indexes/03-create-index.sql
+++ Codes/Indexes/03-create-index.sql
@@ -1,3 +1,5 @@
 -- CREATE INDEX idx_salary ON users (salary);
 
+-- CREATE UNIQUE INDEX idx_salary ON users (salary); -- Unique index to ensure no duplicate salaries it is same as using UNIQUE constraint in table definition for salary column...
+
 -- DROP INDEX idx_salary;
